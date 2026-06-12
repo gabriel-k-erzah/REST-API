e1366b74b5aa915b959650fd7840f8a08d9f987b
Gabriel <gabrielerzah@gmail.com>
2026-06-12 16:11:19 +0100

    added an empty list in which the user can add items as a str, and get items using item_id,
    to do:
    look at helpful error messages

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+items = []
+
 @app.get("/")
 def root():
     return {"message": "Welcome to the API!"}
+
+@app.post("/items")
+def creat_item(item: str):
+    items.append(item)
+    return item
+
+@app.get("/items/{item_id}")
+def get_item(item_id: int) -> str: #auto conversion from fastapi
+    item = items[item_id]
+    return item
+
+
